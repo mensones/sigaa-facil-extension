0995cf1596d903ca967595702f9bd5f7ba974481
rafaelcds <rafaelcds@e6a106cf-b425-0ddd-87a1-1ec0fdbe8a3d>
2011-07-18 16:06:57 +0000

    moving...

---
+++ SIGAA_Facil_Firefox/srcExtension/defaults/preferences/prefs.js
@@ -0,0 +1,3 @@
+pref("extensions.sigaafacil.autorun", true);
+pref("extensions.sigaafacil.restringir-dominios", false);
+pref("extensions.sigaafacil.lista-dominios", "ufc.br \nufrn.br \nufersa.edu.br");
